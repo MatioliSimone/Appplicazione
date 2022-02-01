10f2faa103e2e34f8b04cbc23dbb5f10d21eb030
MatioliSimone <simone.mattioli@marconirovereto.it>
2022-02-01 15:33:44 +0100

    modifiche app.py

--- app.py
+++ app.py
@@ -1,36 +1,53 @@
-import json
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+IMAGES_PATH = "/static/img"
+POSTER_FORMAT_NAME = "{}_Poster.jpg"
+
+def getImageSrc(image):
+    return IMAGES_PATH+"/"+image
+
+def filterMovie(movie):
+    if "poster" in movie.keys():
+        movie["poster"] = getImageSrc(movie["poster"])
+    if "director_avatar" in movie.keys():
+        movie["director_avatar"] = getImageSrc(movie["director_avatar"])
+    return movie
+
+def filterMovies(movies):
+    return [ filterMovie(m.copy()) for m in movies]
+
 
 app = Flask(__name__)
+CORS(app)
 
 movies = [
     {
         "name": "The Shawshank Redemption",
         "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
-        "genres": ["Drama"]
+        "director": "Frank Darabont",
+        "director_avatar": "Frank-Darabont_avatar.jpeg",
+        "genres": ["Drama"],
+        "poster": "Shawshank-Redemption_Poster.jpg",
+        "release_date": "September 10, 1994",
     },
     {
        "name": "The Godfather ",
        "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
-       "genres": ["Crime", "Drama"]
+       "director": "Francis Ford Coppola",
+       "director_avatar": "Francis-Ford-Coppola_avatar.jpg",
+       "genres": ["Crime", "Drama"],
+       "poster": "The-Godfather_Poster.jpg",
+       "release_date": "March 14, 1972",
     }
 ]
 
-def getImageSrc(image):
-    return IMAGES_PATH + "/" + image
+@app.route("/movies")
+def show_movies():
+    return jsonify(filterMovies(movies))
 
-def filterMovie(movie):
-    if "poster" in movies.keys():
-        movie["poster"] = getImageSrc(movie["director_avatar"])
-    if "director_avatar" in movies.keys():
-        movie["director_avatar"] = getImageSrc(movie["director_avatar"])
-    return movie  
-          
-def hello():
-     return jsonify(movies)
 
 @app.route('/movies', methods=['POST'])
-
 def add_movie():
     movie = request.get_json()
     movies.append(movie)
@@ -44,7 +61,8 @@ def update_movie(index):
 
 @app.route('/movies/<int:index>', methods=['DELETE'])
 def delete_movie(index):
+    movie = movies[index]
     movies.pop(index)
-    return jsonify, 200
+    return jsonify(movie), 200
 
 app.run()
