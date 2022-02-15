0ba24dff4d056c4a7e12f2626789c99f9b51d3d8
MatioliSimone <simone.mattioli@marconirovereto.it>
2022-02-15 14:14:05 +0000

    Configurazione vecchia

---
+++ init-mongo.js
@@ -0,0 +1,11 @@
+db.createUser(
+    {
+        user: "flask",
+        pwd: "ciao",
+        roles: [ {
+            role: "dbOwner",
+            db: "moviedb"
+        }, "dbOwner" ],
+        mechanisms: [ "SCRAM-SHA-1"],
+    }
+)
