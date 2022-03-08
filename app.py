775b5581218b4f0d3496f9f2521a9922aa73967e
MatioliSimone <simone.mattioli@marconirovereto.it>
2022-03-08 14:19:48 +0000

    update file app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 #~/movie-bag/app.py
 
-from flask import Flask, request, Response
+from flask import Flask
 from database.db import initialize_db
-from database.models import Movie
-import json
+from resources.movie import movies
 
 app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
  'host': 'mongodb://localhost/movie-bag'
-}
+ }
 
 initialize_db(app)
+app.register_blueprint(movies)
 
 app.run()
